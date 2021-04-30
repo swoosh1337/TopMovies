c1bb192a85aae958db6cf4f3cabc3cc3ff6b8667
Tamaza <igrigolia@wpi.edu>
2021-04-30 14:18:54 +0400

    add overview

--- script.js
+++ script.js
@@ -1,45 +1,126 @@
-const apiUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
-const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI =
-    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
-// Selecting our Elements.
-const main = document.getElementById("main");
-const form = document.getElementById("form");
-const search = document.getElementById("search");
-/* call the showMovies function that requests the movie data from the Api using fetch.
- Then it puts those data in the main HTML tag by creating elments for those data. */
-showMovies(apiUrl);
-function showMovies(url){
-    fetch(url).then(res => res.json())
-    .then(function(data){
-    data.results.forEach(element => {
-      // Creating elemnts for our data inside the main tag. 
-        const el = document.createElement('div');
-        const image = document.createElement('img');
-        const text = document.createElement('h3');
-        const rating = document.createElement('h3');
+const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
-        text.innerHTML = `${element.title}`;
-        image.src = IMGPATH + element.poster_path;
-        rating.innerHTML =`${element.vote_average}`;
-        el.appendChild(image);
-        el.appendChild(text);
-        el.appendChild(rating);
-        main.appendChild(el);
-    }); 
-});
+const main = document.getElementById('main');
+const form =  document.getElementById('form');
+const search = document.getElementById('search');
+
+
+
+function highlightSelection() {
+    const tags = document.querySelectorAll('.tag');
+    tags.forEach(tag => {
+        tag.classList.remove('highlight')
+    })
+    clearBtn()
+    if(selectedGenre.length !=0){   
+        selectedGenre.forEach(id => {
+            const hightlightedTag = document.getElementById(id);
+            hightlightedTag.classList.add('highlight');
+        })
+    }
+
 }
 
-// Prevent the Form from submitting if the search bar is empty.
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    main.innerHTML = '';
-     
-    const searchTerm = search.value;
- /* Adding the value wriiten in the search bar to the search Api,
-    in order to get the movies we search for. */
-    if (searchTerm) {
-        showMovies(SEARCHAPI + searchTerm);
-        search.value = "";
+function clearBtn(){
+    let clearBtn = document.getElementById('clear');
+    if(clearBtn){
+        clearBtn.classList.add('highlight')
+    }else{
+            
+        let clear = document.createElement('div');
+        clear.classList.add('tag','highlight');
+        clear.id = 'clear';
+        clear.innerText = 'Clear x';
+        clear.addEventListener('click', () => {
+            selectedGenre = [];
+            setGenre();            
+            getMovies(API_URL);
+        })
+        tagsEl.append(clear);
     }
-});
+    
+}
+
+getMovies(API_URL);
+
+function getMovies(url) {
+
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        if(data.results.length !== 0){
+            showMovies(data.results);
+        }else{
+            main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
+        }
+       
+    })
+
+}
+
+
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+             <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        
+        `
+
+        main.appendChild(movieEl);
+    })
+}
+
+
+function getColor(vote) {
+    if(vote>= 8){
+        return 'green'
+    }else if(vote >= 5){
+        return "orange"
+    }else{
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+ 
+  
+    if(searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(API_URL);
+    }
+
+})
+
+// // Prevent the Form from submitting if the search bar is empty.
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     main.innerHTML = '';
+     
+//     const searchTerm = search.value;
+//  /* Adding the value wriiten in the search bar to the search Api,
+//     in order to get the movies we search for. */
+//     if (searchTerm) {
+//         showMovies(SEARCHAPI + searchTerm);
+//         search.value = "";
+//     }
+// });
